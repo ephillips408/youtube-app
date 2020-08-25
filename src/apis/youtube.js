b21833e507fc826fca17a0a3bd55cb6123542bd1
Eric Phillips <e.phillips.59715@gmail.com>
2020-08-25 10:35:19 -0700

    Initial commit

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyChEszlNK_RpU-MxNQVNakrw-OI8xhHw3w";
+const KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
